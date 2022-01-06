38e30a7c0d742229f2c61a0a1167adb7315a99d3
JyotiParit <jyotiparit88@gmail.com>
2022-01-06 15:31:35 +0530

    First 100 Fibonnaci num

---
+++ Fibb100.ts
@@ -0,0 +1,8 @@
+function fib(num){
+    const fibarray=[0,1];
+    for(var i=2;i<num; i++){
+        fibarray.push(fibarray[i-2]+fibarray[i-1]);
+    }
+    return fibarray;
+}
+console.log(fib(100));
